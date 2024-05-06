39e2f069cf75c763b4f815bd0bdabb0f83649c9e
Diana <dianafrancitorraferran@gmail.com>
2024-05-06 16:24:05 +0200

    prova funcional al main, falta fer els menus per poder entrar els valors

--- Sprint_03/src/main/java/tasca_01/n2exercici1/Main.java
+++ Sprint_03/src/main/java/tasca_01/n2exercici1/Main.java
@@ -2,13 +2,22 @@ package tasca_01.n2exercici1;
 
 
 import org.w3c.dom.ls.LSOutput;
+import tasca_01.n2exercici1.exceptions.InvalidOption;
 import tasca_01.n2exercici1.factories.AbstractContactFactory;
 import tasca_01.n2exercici1.factories.InternationalFactory;
 import tasca_01.n2exercici1.factories.NationalFactory;
 
 public class Main {
-    AbstractContactFactory a = new AbstractContactFactory();
-    InternationalFactory i = a.getInternational();
-    NationalFactory n = a.getNational();
-    System.out.println();
+    public static void main(String[] args) {
+        AbstractContactFactory a = new AbstractContactFactory();
+        InternationalFactory i = a.getInternational();
+        NationalFactory n = a.getNational();
+        try{
+            System.out.println(n.getContact("Arturo",45678912,"calle de la calabaza",5,2,1,"08045","MAD"));
+        }catch(InvalidOption e){
+            System.out.println(e.getMessage());
+        }
+
+    }
+
 }
