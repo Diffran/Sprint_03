0c52187d823650612263f77f1e4dbfa7bd6c9e6d
Diana <dianafrancitorraferran@gmail.com>
2024-05-15 12:13:45 +0200

    ksdjklajdf

--- Sprint_03/src/main/java/tasca_02/n2exercici1/paymentMethod/BankTransfer.java
+++ Sprint_03/src/main/java/tasca_02/n2exercici1/paymentMethod/BankTransfer.java
@@ -1,11 +1,12 @@
 package tasca_02.n2exercici1.paymentMethod;
 
 public class BankTransfer implements CallbackPayment{
+    static{
+        System.out.println("processing Bank Payment ");
+    }
     @Override
     public void payment(String paymentMethod) {
-        static{
-            System.out.println("processing Bank Payment ");
-        }
+
 
     }
 }
